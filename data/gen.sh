0e1a333efa56a9d583382166ca65e1c38337ed29
Anastasios Pechlivanidis <86526783+Pechlivanidis@users.noreply.github.com>
2025-05-02 16:44:24 +0300

    removed gen.sh, no longer needed

--- data/gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir output_folder
-
-awk '/>/{printf("+Example-01-%006d ", i++)}1'  example.fasta > output_folder/Example.faa; 
-sed -i 's/>//g' output_folder/Example.faa; 
-sed -i 's/+/>/g' output_folder/Example.faa; 
